2b40361cedc6af2da0c25f9585640ef87794ad13
ViswaaE <viswaaeswaran@gmail.com>
2025-09-02 07:35:37 +0530

    basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import Student from "./Student";
+import About from "./components/about";
+import Home from "./components/home";
+// import Student from "./Student";
+import { Routes, Route, Link } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <>
-      <Student name="viswaa" age={21} isStudent={true} />
+    <div >
+      <Link to="/">home</Link>
+      <Link to="/about">about</Link>
+    </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
